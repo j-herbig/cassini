2c0559a4e4feb8ded3095482b207d168dcb429bb
Johannes Herbig <94560419+j-herbig@users.noreply.github.com>
2022-05-26 21:46:48 +0200

    added eda, makefile, readme and updated requirements (#2)

--- db_setup.py
+++ db_setup.py
@@ -6,20 +6,23 @@ from tqdm import tqdm
 import download_params
 import db_params
 
-def get_all_csvs(path: str) -> list:
+def get_all_csvs(path: str, path_excluded: str = None) -> list:
     """Gets the paths to all csv files in the given path.
 
     Args:
         path (str): Path to search csv files in.
+        path_excluded (str): Path excluded from search.
 
     Returns:
         list: Contains paths to all found csv files.
     """
+    # find all csv files in a given directory, subdirectories can be excluded (path_axclude)
     csv_files = []
     for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.split(".")[-1] == "csv":
-                csv_files.append(os.path.join(root, file))
+        if not root == path_excluded:
+            for file in files:
+                if file.split(".")[-1] == "csv":
+                    csv_files.append(os.path.join(root, file))
 
     return csv_files
 
@@ -34,6 +37,7 @@ def get_col_types_for_db(df: pd.DataFrame, primary_key: str = None) -> list:
     Returns:
         list: Database data types for all columns of the given dataframe.
     """
+    # Get dtypes of all columns in the given dataframe and translate them to database data types
     col_types = []
     for col in df.columns:
         data_type = ""
@@ -43,6 +47,7 @@ def get_col_types_for_db(df: pd.DataFrame, primary_key: str = None) -> list:
             data_type = "REAL"
         elif df[col].dtype == object:
             data_type = "TEXT"
+        # if the given column was specified as primary key, add the corresponding string
         if col == primary_key:
             data_type = data_type + " PRIMARY KEY"
         col_types.append(data_type)
@@ -60,9 +65,12 @@ def make_table_command(table_name: str, columns: list, col_types: list) -> str:
     Returns:
         str: SQL command to create the table.
     """
+    # make the beginning of the command using the table name
     command = f"CREATE TABLE {table_name} ("
+    # add for every column  name and data type 
     for i, col in enumerate(columns):
         command = command + f"{col} {col_types[i]}, "
+    # return string without the last comma and whitespace and close the parenthesis
     return command[:-2] + ")"
 
 
@@ -76,12 +84,14 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
     Returns:
         dict: Table names and corresponding column names of the database.
     """
+    # define columns that are used as IDs
     cols_id = [
         "FlightDate",
         "Reporting_Airline", 
         "OriginAirportID", 
         "DestAirportID",
     ]
+    # define columns for the time table
     cols_time = [
         "Year", 
         "Quarter", 
@@ -90,6 +100,7 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
         "DayOfWeek", 
         "FlightDate",
     ]
+    # define columns for airline table
     cols_airline = [
         "Reporting_Airline",
         "DOT_ID_Reporting_Airline",
@@ -97,6 +108,7 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
         # "Tail_Number",
         # "Flight_Number_Reporting_Airline",
     ]
+    # define columns of origin airport
     cols_airport_origin = [
         "OriginAirportID",
         "OriginAirportSeqID",
@@ -108,6 +120,7 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
         "OriginStateName",
         "OriginWac",
     ]
+    # define columns of destination airport
     cols_airport_dest = [
         "DestAirportID",
         "DestAirportSeqID",
@@ -119,6 +132,7 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
         "DestStateName",
         "DestWac",
     ]
+    # from the definitions before, get columns for the flight table
     cols_flight = [
         col for col in df.columns \
         if (col not in cols_time \
@@ -127,10 +141,12 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
         and col not in cols_airport_dest) \
         or col in cols_id
     ]
+    # derive columns for airport table from the destination airport column list
     cols_airport = [
         col[4:] if col != "Dest" else "Airport" for col in cols_airport_dest
     ]
 
+    # add commands to create tables to a list
     make_table_commands = []
     make_table_commands.append(
         make_table_command(
@@ -153,17 +169,22 @@ def make_database(database_path: str, df: pd.DataFrame) -> dict:
         )
     )
 
+    # open connection and cursor to the database 
     conn = sqlite3.connect(database_path)
     crsr = conn.cursor()
 
+    # make tables using the commands created above
     for cmd in make_table_commands:
         try:
             crsr.execute(cmd)
         except Exception as e:
             print(e)
+    
+    # close cursor and connection
     crsr.close()
     conn.close()
 
+    # return tables and corresponding column names
     return {"time_period": cols_time, "airports": cols_airport, "airlines": cols_airline, "flights": cols_flight}
 
 
@@ -177,55 +198,98 @@ def get_uniques(df: pd.DataFrame, column_uniques: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Dataframe with unique values of column_uniques apearing only once.
     """
-    # uniques = df[column_uniques].unique()
+    # add the first row for each unique value in column_uniques to a dataframe
     df_uniques = pd.DataFrame()
     for val in df[column_uniques].unique():
         df_uniques = pd.concat([df_uniques, df[df[column_uniques]==val].iloc[:1,:]])
     return df_uniques
 
 
+def transfer_look_up_table_to_db(path_file: str, db_connection: sqlite3.Connection):
+    """Transfers data of look up tables to a database.
+
+    Args:
+        path_file (str): Path to the csv file containing a look up table.
+        db_connection (sqlite3.Connection): Connection to a database to transfer data to.
+    """
+    # use file name without extension as table name
+    table_name = path_file.split("/")[-1].split(".")[0]
+
+    # write data to database
+    pd.read_csv(path_file).to_sql(table_name, db_connection, if_exists="replace", index=False)
+
+
 if __name__ == "__main__":
+    # get parameters from db_params.py
     db_name = db_params.DB_NAME
     db_path = db_params.DB_PATH
     db_path = os.path.join(db_path, db_name)
-    csv_files = get_all_csvs(download_params.DOWNLOAD_PATH)
+
+    # get csv files from the download folder excluding look up tables
+    data_files = get_all_csvs(download_params.DOWNLOAD_PATH, download_params.DOWNLOAD_PATH_LOOKUP)
+
+    # make empty dataframes to append data from the csv files afterwards
     df_time_periods = pd.DataFrame()
     df_airports = pd.DataFrame()
     df_airlines = pd.DataFrame()
 
-    with tqdm(total=len(csv_files)) as pbar:
+    print("transfer data to database...")
 
-        for i, csv_file in enumerate(csv_files):
+    # visualize file processing with a progress bar
+    with tqdm(total=len(data_files)) as pbar:
+        
+        # process the data files found before
+        for i, csv_file in enumerate(data_files):
+
+            # read one file into a dataframe
             df = pd.read_csv(csv_file, low_memory=False)
+
+            # drop columns containing 'Unnamed'
             for col in df.columns:
                 if "Unnamed" in col:
                     df.drop(col, axis=1, inplace=True)
+            
+            # in the first loop make the databse and connect to it
             if not i:
                 db_tables = make_database(db_path, df)
                 conn = sqlite3.connect(db_path)
-            # add_data_to_db("on_time_performance", db_path, df, col_groups)
+
+            # process flights
+            # append data of flights to database
             df[db_tables["flights"]].to_sql("flights", conn, if_exists="append", index=False)
             
-            # airports
+            # process airports
+            # filter for data of destination and origin airports
             dest_airports = df[[f"Dest{col}" if col != "Airport" else "Dest" for col in db_tables["airports"]]]
             origin_airports = df[[f"Origin{col}" if col != "Airport" else "Origin" for col in db_tables["airports"]]]
+            # set their column names equal to the column names of the database airport table to simplify appending
             dest_airports.columns = origin_airports.columns = db_tables["airports"]
-            airports = pd.concat([dest_airports, origin_airports])
-            airports = get_uniques(airports, "AirportID")
-            df_airports = pd.concat([df_airports, airports])
+            # add origin and destination airports to the airports dataframe and only keep unique values
+            df_airports = get_uniques(pd.concat([df_airports, dest_airports, origin_airports]), "AirportID")
 
-            # airlines
+            # process airlines
+            # add only uniques of airlines to the airline dataframe to keep it small 
             airlines = get_uniques(df[db_tables["airlines"]], "Reporting_Airline")
             df_airlines = pd.concat([df_airlines, airlines])
 
-            # time periods
+            # process time periods
+            # add only uniques of time periods to the time period dataframe to keep it small 
             time_periods = get_uniques(df[db_tables["time_period"]], "FlightDate")
             df_time_periods = pd.concat([df_time_periods, time_periods])
+            
+            # update progress bar
             pbar.update(1)
 
-
+    # add the time periods dataframe to the database
     df_time_periods.to_sql("time_period", conn, if_exists="append", index=False)
+    # add uniques of airport and airlines dataframe to the database
     get_uniques(df_airports, "AirportID").to_sql("airports", conn, if_exists="append", index=False)
     get_uniques(df_airlines, "Reporting_Airline").to_sql("airlines", conn, if_exists="append", index=False)
 
+    print("transfer look up tables to database...")
+    # add look up tables to the database
+    for look_up_table in tqdm(get_all_csvs(download_params.DOWNLOAD_PATH_LOOKUP)):
+        transfer_look_up_table_to_db(look_up_table, conn)
+
+    # close database connection
     conn.close()
--- download_and_unzip.py
+++ download_and_unzip.py
@@ -3,6 +3,7 @@ import requests
 import zipfile
 from tqdm import tqdm
 import download_params
+from bs4 import BeautifulSoup as bs
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -15,30 +16,47 @@ def get_download_params() -> tuple:
         tuple: Parameters for downloading, saving and unzipping files.
     """
     return (
-        download_params.URL_ROOT, 
-        download_params.DOWNLOAD_PATH, 
-        download_params.YEARS, 
-        download_params.ZIP_PREFIX, 
-        download_params.CSV_PREFIX
+        download_params.URL_ROOT,
+        download_params.URL_ZIP_PREFIX,
+        download_params.URL_WEBSITE,
+        download_params.DOWNLOAD_PATH,
+        download_params.DOWNLOAD_PATH_LOOKUP ,
+        download_params.YEARS,
+        download_params.ZIP_PREFIX,
+        download_params.CSV_PREFIX,
     )
 
 
-def download_file(url_dl: str, path: str, file_name: str):
+def download_file(url_dl: str, path: str, file_name: str = None):
     """Downloads a file and writes it to disk.
 
     Args:
         url_dl (str): Download URL of the file. 
         path (str): Path to save the file in.
-        file_name (str): Name of the file.
+        file_name (str): Name of the file, if None, file name is read from header of response. Defaults to None.
     """
-    # check if file already exists
+    
+    # if path doesn't exist, make it
     if not os.path.exists(path):
         os.mkdir(path)
 
+    # request download url
     response = requests.get(url_dl, timeout=50, verify=False)
+
+    # if the file name was not specified, get it from download server
+    if not file_name:
+        try:
+            file_name = response.headers["Content-Disposition"][
+                response.headers["Content-Disposition"].find("filename=") + len("filename="):
+            ]
+        except Exception as e:
+            print(f"file name could not be read from server for {url_dl}")
+            print("download gets skipped")
+
     # write file to disk
-    with open(os.path.join(path, file_name), 'wb') as f:
-        f.write(response.content)
+    if file_name:
+        with open(os.path.join(path, file_name), 'wb') as f:
+            f.write(response.content)
 
 
 def unzip_file(path_zip: str, file_to_extract: str, path_target: str = None, delete_zip: bool = False):
@@ -50,12 +68,15 @@ def unzip_file(path_zip: str, file_to_extract: str, path_target: str = None, del
         path_target (str, optional): Path to save the unzipped file in. Defaults to None.
         delete_zip (bool, optional): Whether or not to delete the zip file after unzipping. Defaults to False.
     """
+    # if target path was specified, make it, if it doesn't exist
+    # else use origin path of zip as target path
     if path_target != None: 
         if not os.path.exists(path_target):
             os.mkdir(path_target)
     else:
         path_target = "/".join(path_zip.split("/")[:-1])
     
+    # if the file to extract doesn't exist, extract from zip file
     if not os.path.exists(os.path.join(path_target, file_to_extract)):
         try:
             with zipfile.ZipFile(path_zip) as z:
@@ -64,6 +85,7 @@ def unzip_file(path_zip: str, file_to_extract: str, path_target: str = None, del
         except Exception as e:
             print(f"Error when unzipping {path_zip}: {e}")
     
+    # delete zip file if necessary
     if delete_zip:
         os.remove(path_zip)
 
@@ -78,24 +100,80 @@ def download_and_unzip_years(url_root: str, path: str, years: int or list, zip_p
         zip_prefix (str): Prefix of the zip files.
         csv_prefix (str): Prefix of the csv files.
     """
+    # if years was passed as integer, make it a list
     if type(years) == int:
         years = [years]
 
+    print("Processing data files...")
+
+    # use a progress bar to visualize file processing
     with tqdm(total=len(years) * 12) as pbar:
+        # download process files for all months in the specified years
         for year in years:
             for month in range(1, 13):
+                # make url, names and path for downloading and naming files
                 url_download = url_root + zip_prefix + f"{year}_{month}.zip"
                 name_zip = zip_prefix + f"{year}_{month}.zip"
                 name_csv = csv_prefix + f"{year}_{month}.csv"
                 path_zip = os.path.join(path, name_zip)
+
+                # download and unzip the file if csv, respectively zip file doesn't exist
+                if not os.path.isfile(os.path.join(path, name_csv)):
+                    if not os.path.isfile(path_zip):
+                        download_file(url_download, path, name_zip)
+                    unzip_file(path_zip, name_csv, path_target=None, delete_zip=True)
                 
-                if not os.path.isfile(path_zip):
-                    download_file(url_download, path, name_zip)
-                unzip_file(path_zip, name_csv, path_target=None, delete_zip=True)
-                # print()
+                # proceed progress bar
                 pbar.update(1)
 
 
+def get_lookup_urls(url_root: str, download_root: str) -> list:
+    """Gets download URLs for llokup tables.
+
+    Args:
+        url_root (str): URL to website to search for look up table download links.
+        download_root (str): Root URL for downloading for downloading look up tables
+
+    Returns:
+        list: Download URLs for look up tables.
+    """
+    # request url and parse it to make download links findable
+    r = requests.get(url_root, timeout=10, verify=False)
+    soup = bs(r.text, features="html.parser")
+    download_urls = []
+
+    # find all download links for look up tables and add it to the list
+    for i, link in enumerate(soup.findAll('a')):
+        try:
+            if soup.select('a')[i].attrs['title'] == "Download Lookup data":
+                download_urls.append(download_root + link.get('href'))
+        except KeyError:
+            continue
+    return download_urls
+
+
+def download_lookup_tables(url_root: str, download_root: str, path: str):
+    """Downloads look up tables as csv files.
+
+    Args:
+        url_root (str): URL to website to search for look up table download links.
+        download_root (str): Root URL for downloading for downloading look up tables.
+        path (str): Path to save look up table csv files.
+    """
+    print("Processing look up tables...")
+
+    # get download links for look up tables and download them
+    for url_dl in tqdm(get_lookup_urls(url_root, download_root)):
+        download_file(url_dl, path)
+
+
 if __name__ == "__main__":
-    url_root, path, years, zip_prefix, csv_prefix = get_download_params()
-    download_and_unzip_years(url_root, path, years, zip_prefix, csv_prefix)
+    # get urls, paths and other parameters necessary to process files
+    url_root, url_zip_prefix, url_website, download_path, download_path_lookup, years, zip_prefix, csv_prefix = get_download_params()
+
+    # download and unzip data of whole years
+    download_and_unzip_years(url_root + url_zip_prefix, download_path, years, zip_prefix, csv_prefix)
+
+    # download look up tables
+    download_lookup_tables(url_root + url_website, url_root, download_path_lookup)
+    
